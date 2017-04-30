76a1f4533da7d1d2465bdc49468234e0f0131e74
KerJen <kerjen01@gmail.com>
2017-04-30 22:00:57 +0300

    Не вес файлы закоммитились, исправил

---
+++ app/src/main/java/com/development/mobile/andromeda/shimmer/OnItemClickListener.java
@@ -0,0 +1,9 @@
+package com.development.mobile.andromeda.shimmer;
+
+/**
+ * Created by Anton on 29.04.2017.
+ */
+
+public interface OnItemClickListener {
+        void onItemClick(ModelItem item);
+}
